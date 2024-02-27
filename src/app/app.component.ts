efe07cba81bf49421f880f8bbac1001ef9ed712f
jayborad <co-20-081@rctiahmedabad.gujgov.edu.in>
2024-02-27 12:35:51 +0530

    initial commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,10 @@
-import { Component } from '@angular/core';
+import { Component, ViewEncapsulation } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
+  encapsulation: ViewEncapsulation.None  // Use this to make styles global (non-scoped)
 })
 export class AppComponent {
   title = 'swiperjs';
--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,11 @@
 import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 
 import { AppModule } from './app/app.module';
+import { register as registerSwiperElement } from 'swiper/element/bundle';
 
-
+registerSwiperElement();
+// register();
 platformBrowserDynamic().bootstrapModule(AppModule)
   .catch(err => console.error(err));
+
+
